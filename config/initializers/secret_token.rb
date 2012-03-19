6d0200ee1876348d965569f813d31099764bbdcd
Hatem Mahmoud <hatem@expressionlab.com>
2012-03-19 18:53:43 +0200

    rails _3.1.0_ new tr8n310

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tr8n310::Application.config.secret_token = '6ae9d30be6a4efd749c1107e276353d2853d792e59e6795d00e94c4ca4824a8c7efb6c028813376500b74868948ea1d2ac5703792d23f54c9dafde8c7e29d3d9'
